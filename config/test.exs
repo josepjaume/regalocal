53b78c028884e85622eca4e896a3d934df13e2c7
Josep Jaume <josepjaume@gmail.com>
2020-04-06 21:32:46 +0200

    Fix test

--- config/test.exs
+++ config/test.exs
@@ -1,12 +1,17 @@
 use Mix.Config
 
-# Configure your database
-config :regalocal, Regalocal.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "regalocal_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+database_url = System.get_env("DATABASE_URL")
+
+if(database_url) do
+  config :regalocal, Regalocal.Repo,
+    url: System.get_env("DATABASE_URL"),
+    pool: Ecto.Adapters.SQL.Sandbox
+else
+  config :regalocal, Regalocal.Repo,
+    database: "regalocal_test",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
